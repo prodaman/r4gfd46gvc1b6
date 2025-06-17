447e55a5c7e93f6a1ba2918c95339e42076a440c
prodaman <115877336+prodaman@users.noreply.github.com>
2025-06-17 16:37:04 +0200

    Update replacex.php

--- replacex.php
+++ replacex.php
@@ -1,8 +1,8 @@
 <?php
 
 $repoPath = __DIR__; // Directorio del repositorio local
-$search = "M. Series";
-$replace = "M+ Hits";
+$search = "M. Cine";
+$replace = "M+ ESTRENOS";
 
 // Buscar archivos .m3u en el repositorio
 $m3uFiles = glob("$repoPath/*.m3u");
